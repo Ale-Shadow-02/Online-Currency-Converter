3013f8c053cd2ffd40705c95f9abd6b4c60af5a4
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2020-11-24 15:15:54 +0400

    Запрграммированы клики по интефктивным элементам, добалена стрелка к сулукту

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,30 @@
 let menuLink = document.querySelectorAll('.menu-list__item--link');
 
-
-
 menuLink.forEach(elem => {
-
-  elem.addEventListener('click', (event) => {
-    event.preventDefault();
-    let target = event.target;
-    console.log('target: ', target);
-
-    // target.classList.add('active');
-    if (!elem.classList.contains('active')) {
-      elem.classList.add('active');
-    } else {
+  elem.addEventListener('click', event => {
+    menuLink.forEach(elem => {
       elem.classList.remove('active');
-    }
+    });
+    elem.classList.add('active');
+  });
+});
+
+let itemSwitcherBox = document.querySelectorAll('.item__switcher--box');
+itemSwitcherBox.forEach(function (elem) {
+  elem.addEventListener('click', function (event) {
+    itemSwitcherBox.forEach(elem => {
+      elem.classList.remove('box-active');
+    });
+    elem.classList.add('box-active');
+  });
+});
+
+let itemSwitcherBox2 = document.querySelectorAll('.item__switcher--box2');
+itemSwitcherBox2.forEach(function (elem) {
+  elem.addEventListener('click', function (event) {
+    itemSwitcherBox2.forEach(elem => {
+      elem.classList.remove('box-active');
+    });
+    elem.classList.add('box-active');
   });
 });
