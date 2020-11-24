2c80e8f0146caf6f5ea9b2349c7434fcc0b4b03c
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2020-11-24 11:52:09 +0400

    Сделана верстка

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 let menuLink = document.querySelectorAll('.menu-list__item--link');
 
+
+
 menuLink.forEach(elem => {
+
   elem.addEventListener('click', (event) => {
     event.preventDefault();
     let target = event.target;
     console.log('target: ', target);
+
     // target.classList.add('active');
     if (!elem.classList.contains('active')) {
       elem.classList.add('active');
