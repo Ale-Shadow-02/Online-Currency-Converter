a6ed52fad838f43354559c63d93792cc1c1f3edf
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2020-11-25 13:23:30 +0400

    Написан скрипт заполнения селекта option акткальной валютой. Скрипт для полусения значений из валюты по умолчанию

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 let menuLink = document.querySelectorAll('.menu-list__item--link');
+//* Делаем активной кнопку меню при нажатии
 
 menuLink.forEach(elem => {
   elem.addEventListener('click', event => {
@@ -8,6 +9,7 @@ menuLink.forEach(elem => {
     elem.classList.add('active');
   });
 });
+//* Делаем активной кнопку выбора валюты при нажатии на секцию "У меня есть"
 
 let itemSwitcherBox = document.querySelectorAll('.item__switcher--box');
 itemSwitcherBox.forEach(function (elem) {
@@ -18,6 +20,7 @@ itemSwitcherBox.forEach(function (elem) {
     elem.classList.add('box-active');
   });
 });
+//* Делаем активной кнопку выбора валюты при нажатии на секцию "Хочу приобрести"
 
 let itemSwitcherBox2 = document.querySelectorAll('.item__switcher--box2');
 itemSwitcherBox2.forEach(function (elem) {
@@ -26,5 +29,53 @@ itemSwitcherBox2.forEach(function (elem) {
       elem.classList.remove('box-active');
     });
     elem.classList.add('box-active');
+  });
+});
+
+//*Converter ====================================================
+
+//*Заполняем option select актуальными названиями валюты ========
+const select = document.querySelector('.box-select__select');
+const select2 = document.querySelector('.select2');
+
+fetch('https://api.ratesapi.io/api/latest')
+  .then(res => res.json())
+  .then(data => {
+    let objRates = data.rates;
+    for (let key in objRates) {
+      let option = document.createElement('option');
+      option.classList.add('box-select__select--option');
+      option.setAttribute('value', key);
+      option.innerHTML = key;
+      select.appendChild(option);
+    }
+    for (let key in objRates) {
+      let option = document.createElement('option');
+      option.classList.add('box-select__select--option');
+      option.setAttribute('value', key);
+      option.innerHTML = key;
+      select2.appendChild(option);
+    }
+  });
+
+//*Получаем текстовае значения валюты по умолчанию =================
+
+let currencyFrom = 'RUB';
+let currencyTo = 'USD';
+
+let switcherBox1 = document.querySelectorAll('.switcher-box-1 ');
+switcherBox1.forEach(el => {
+  el.addEventListener('click', event => {
+    let target = event.target;
+    currencyFrom = target.textContent || target.innerText;
+    console.log('currencyFrom: ', currencyFrom);
+  });
+});
+let switcherBox2 = document.querySelectorAll('.switcher-box-2 ');
+switcherBox2.forEach(el => {
+  el.addEventListener('click', event => {
+    let target = event.target;
+    currencyTo = target.textContent || target.innerText;
+    console.log('currencyTo: ', currencyTo);
   });
 });
